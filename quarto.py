38ce4aab763d203a148b19e7ef83ae77d4fd9cea
Edilson Silva <edilson@yahoo.com.br>
2024-09-25 08:27:52 -0300

    Primeiros códigos em python

---
+++ quarto.py
@@ -0,0 +1,10 @@
+numero1 = input("Digite um valor:")
+numero2 = input("Digite outro valor:")
+# usamos o comando int para converter as variáveis
+# numero1 e numero2 em valores inteiros, pois
+# o retorno do comando input é sempre uma 
+# string
+
+resultado = int(numero1) + int(numero2)
+
+print(resultado)
